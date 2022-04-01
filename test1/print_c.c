3b2cd13f65f1b6a9eb60e463f3595b163e8c739d
IngMariale <4725@holbertonschool.com>
2022-03-31 21:02:11 -0500

    printc y mainh

---
+++ test1/print_c.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *  * print_c - A function that prints a char
+ *   * @list: Character
+ *    * Return: Always 1 (Success)
+ *     */
+int print_c(va_list list)
+{
+		char ch = (char)va_arg(list, int);
+
+			_putchar(ch);
+				return (1);
+}
+
